c0328a2a85d8cb932961287a1fbac7747c59cdfe
Anderson Santos <andersonfarin@gmail.com>
2021-10-05 23:01:09 -0300

    feat(app-store-api): Adding controller for App

--- src/main/java/br/com/rogalabs/appstoreapi/controllers/AppController.java
+++ src/main/java/br/com/rogalabs/appstoreapi/controllers/AppController.java
@@ -1,10 +1,10 @@
 package br.com.rogalabs.appstoreapi.controllers;
 
+import br.com.rogalabs.appstoreapi.controllers.dto.AppRequest;
 import br.com.rogalabs.appstoreapi.controllers.dto.AppResponse;
+import br.com.rogalabs.appstoreapi.domain.App;
 import br.com.rogalabs.appstoreapi.repositories.AppRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,12 +23,44 @@ public class AppController {
         this.appRepository = appRepository;
     }
 
-    @GetMapping("/")
+    @GetMapping("/all")
     public List<AppResponse> findAll() {
         var apps = appRepository.findAll();
         return apps
                 .stream()
                 .map(AppResponse::converter)
                 .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}")
+    public AppResponse findById(@PathVariable("id") Long id) {
+        var app = appRepository.getById(id);
+        return AppResponse.converter(app);
+    }
+
+    @PostMapping("/")
+    public void addNewApp(@RequestBody AppRequest appRequest) {
+        //TODO save category
+        //TODO save publisher
+        var newApp = new App();
+        newApp.setName(appRequest.getName());
+        newApp.setDescription(appRequest.getDescription());
+        newApp.setPrice(newApp.getPrice());
+        appRepository.save(newApp);
+    }
+
+    @PutMapping("/{id}")
+    public void updateApp(@PathVariable("id") Long id, @RequestBody AppRequest appRequest) throws Exception {
+        var app = appRepository.findById(id);
+
+        if (app.isPresent()) {
+            var appToUpdate = app.get();
+            appToUpdate.setName(appToUpdate.getName());
+            appToUpdate.setDescription(appToUpdate.getDescription());
+            appToUpdate.setPrice(appToUpdate.getPrice());
+        } else {
+            //TODO create an customizable exception
+            throw new Exception("App not found");
+        }
     }
 }
---
+++ src/main/java/br/com/rogalabs/appstoreapi/controllers/dto/AppRequest.java
@@ -0,0 +1,38 @@
+package br.com.rogalabs.appstoreapi.controllers.dto;
+
+import br.com.rogalabs.appstoreapi.domain.App;
+
+/**
+ * @author Anderson on 05/10/2021
+ * @project app-store-api
+ */
+public class AppRequest {
+
+    private String name;
+    private String description;
+    private float price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
